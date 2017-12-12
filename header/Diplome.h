339f1864d409adb10f68e24555a777779900b79d
T3nd0 <ange.pgt@outlook.fr>
2017-12-12 14:35:47 +0100

    Ajout Diplome.h et implementation

---
+++ header/Diplome.h
@@ -0,0 +1,28 @@
+#ifndef DIPLOME_H
+#define DIPLOME_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Diplome {
+private:
+    string code;
+    string intitule;
+    string dateObt;
+    string lieuObt;
+
+public:
+    Diplome(string, string, string, string);
+    string getCode();
+    string getIntitule();
+    string getDateObt();
+    string getLieuObt();
+    void setCode(string);
+    void setIntitule(string);
+    void setDateObt(string);
+    void setLieuObt(string);
+
+};
+
+#endif
